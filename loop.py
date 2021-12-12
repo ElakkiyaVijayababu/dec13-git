7c3c76c66cac4e9392cf9e0e7dce2ea5290351dc
“ElakkiyaVijayababu” <ennlucky@gmail.com>
2021-12-12 03:08:42 +0000

    fourth commit -leenu

--- loop.py
+++ loop.py
@@ -1,9 +1,13 @@
+# without end command after print a the solution will be step by step.
 '''a=50
 while a<=100:
     print(a)
     a=a+1
 print("code completed")
 '''
+
+#the end '' denotes the solution single line.
+
 a=50
 while a<=100:
     print(a, end=' ')
