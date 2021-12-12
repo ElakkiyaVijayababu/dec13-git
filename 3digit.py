5e62e8123be7b4b3f168cc3bbe434bf6f450e603
“ElakkiyaVijayababu” <ennlucky@gmail.com>
2021-12-12 02:43:25 +0000

    first commit -leenu

---
+++ 3digit.py
@@ -0,0 +1,13 @@
+print("enter the first number:")
+a=input()
+print("enter the second number:")
+b=input()
+print("enter the Third number:")
+c=input()
+if(a>b)and(a>c):
+    print("The Greatest number is a:",a)
+elif(b>c)and(b>a):
+    print("The Greatest number is b:",b)
+else:
+    print("The Greatest number is c:",c)
+print("findings done.")
