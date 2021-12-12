848a698b5a3de8dd57602b384c99696b64a663e0
“ElakkiyaVijayababu” <ennlucky@gmail.com>
2021-12-12 02:50:26 +0000

    second commit -leenu

---
+++ basicpython.py
@@ -0,0 +1,70 @@
+'''a=10
+b=10.5
+c='leena'
+d=True
+print(a)
+print(b)
+print(c)
+print(d)
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+'''
+'''print("enter the first number:")
+e=input()
+print("enter the second number:")
+f=input()
+g=float(int(e) + int(f))
+print("Total number of both values:",g)
+'''
+'''
+and =multiply
+or=addition
+not=inverse
+'''
+print("enter the first number:")
+a=input()
+print("enter the second number:")
+b=input()
+if a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+print("Thank you for approach the code")
+
+
+
+  
+
+'''
+print("enter the first number:")
+a=input()
+print("enter the second number:")
+b=input()
+print("enter the Third number:")
+c=input()
+if(a>b)and(a>c):
+    print("The Greatest number is:",a)
+elif(b>c)and(b>a):
+    print("The Greatest number is:",b)
+else:
+    print("The Greatest number is:",c)
+print("findings done.")
+'''
+a=50
+for a<=100:
+    print(a)
+    a=a+1
+print("code completed")
+
+
+
+
+
+
+
+
+
+
+''
